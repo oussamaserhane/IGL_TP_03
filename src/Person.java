886d5c05b9126955edb328b20ad17f4cf3d71f63
Siham1602 <100164466+Siham1602@users.noreply.github.com>
2023-01-16 10:42:59 +0100

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -4,6 +4,14 @@ public class Person {
 
 	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	private String last_name;
 
 	private int age;
