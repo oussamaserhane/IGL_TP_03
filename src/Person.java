bc79bcc447ca247dd9095d76a9c0729a2a0b1dc7
Nour2k1 <100165409+Nour2k1@users.noreply.github.com>
2023-01-16 10:06:47 +0100

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -10,4 +10,13 @@ public class Person {
 
 	private String gender;
 
+	public Person() {
+	}
+
+	public Person(String name, String last_name, int age, String gender) { // <----- To added
+		this.name = name;
+		this.last_name = last_name;
+		this.age = age;
+		this.gender = gender;
+	}
 }
